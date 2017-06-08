397ee4ed54d472d8b018f3c7d1dbd84b89edaf91
Christina Curlette <cmcurlette@gmail.com>
2017-06-08 13:35:49 -0400

    Initial commit, untested.

---
+++ app.py
@@ -0,0 +1,87 @@
+from flask import Flask, request, session, url_for, redirect
+import glob
+import os
+from werkzeug.utils import secure_filename
+
+from bayesdb_pred_prob import compute_pred_prob
+
+app=Flask(__name__)
+
+app.secret_key = "P\x11]\xe5\xb6\xf6r=\xc8a\x11O\xcf\x12\xa2W4\x9d\xbfm&\xef6#"
+
+UPLOAD_FOLDER = './tmp/'
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
+# only allow csvs
+ALLOWED_EXTENSIONS = set(['csv'])
+
+# store column names globally, makes it easier to pass them into BayesDB
+col_names = []
+
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1] in ALLOWED_EXTENSIONS
+
+@app.route("/", methods=['GET', 'POST'])
+def setup():
+    if request.method == 'POST':
+        if 'bdb' in request.form.keys() and request.form['bdb'] != '':
+            session['bdb'] = request.form['bdb']
+            return redirect(url_for('setup'))
+        if 'file' in request.files.keys() and request.files['file'].filename != '':
+            # remove old files - only deal with one csv at a time
+            files = glob.glob(app.config['UPLOAD_FOLDER'] + '*')
+            for f in files:
+                os.remove(f)
+            file = request.files['file']
+            if file and allowed_file(file.filename):
+                global col_names
+                # column names are in the first row
+                col_names = request.get_array()[0]
+                filename = secure_filename(file.filename)
+                session['filename'] = filename
+                file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+            return redirect(url_for('setup'))
+    else:
+        if 'filename' not in session.keys():
+            session['filename'] = ''
+        if 'bdb' not in session.keys():
+            session['bdb'] = ''
+        return '''
+            <!doctype html>
+            <title><center>Upload an excel file</title>
+            <link href="./static/bootstrap.min.css" rel="stylesheet" media="screen">
+            <h1><center><br>Anomaly detection with BayesDB</h1>
+            <br><br>
+            <form action="" method=post>
+                <p><center>Set bdb file name of desired bdb:</p>
+                <input type="text" name="bdb">
+                <input type="submit" name="bdb" value="Set">
+            </form>
+            <p><center>%s</p><br>
+            <form action="" method=post enctype=multipart/form-data><center><p>
+                <p>Select a csv to upload:</p>
+                <input type=file name=file><input type=submit value=Upload>
+            </form>
+            <p><center>%s</p><br>
+            <form action="/analyze" method=post>
+                 <input type=submit value="Analyze data">
+            </form><br>
+            <form action="/export" method=post>
+                 <input type=submit value="Export result">
+            </form>
+            ''' % ("bdb set to " + session['bdb'] if session['bdb'] != '' else '', "Uploaded " + session['filename'] if session['filename'] != '' else '')
+
+@app.route("/analyze", methods=['POST','GET'])
+def analyze():
+    global col_names
+    # the following call creates a new csv with pred. probs. added
+    compute_pred_prob(session['filename'], col_names, session['bdb'])
+    return redirect(url_for('setup'))
+
+@app.route("/export", methods=['POST','GET'])
+def export():
+    return send_from_directory(directory=app.config['UPLOAD_FOLDER'], filename='%s_processed.csv' %(os.path.splitext(os.path.basename(session['filename']))[0]))
+
+if __name__ == "__main__":
+    app.run(host='localhost', port=5000)
---
+++ bayesdb_pred_prob.py
@@ -0,0 +1,42 @@
+import bayeslite
+from iventure.bql_utils import cursor_to_df
+
+import copy
+import csv
+import os
+
+def compute_pred_prob(filename, bdb_name, col_names, table="satellites_t", population="satellites_p", columm="Apogee_km", start_row=1):
+	# obtain a pointer to the bdb file
+	bdb = bayeslite.bayesdb_open('../%s' %(bdb_name))
+
+	# insert the data into the table of interest
+	create_table_query = "CREATE TABLE user_input FROM %s" % ("'./tmp/%s'" %(filename))
+	bdb.execute(create_table_query)
+
+	col_names_list = ', '.join(col_names)
+	insert_data_query = "INSERT INTO %s (%s) SELECT %s FROM user_input" %(table, col_names_list, col_names_list)
+	bdb.execute(insert_data_query)
+
+	# we need to know how many rows the table of interest had before we append
+	# more rows
+	num_rows_query = "SELECT COUNT(*) FROM %s" %(table)
+	num_rows_cursor = bdb.execute(num_rows_query)
+	num_rows_df = cursor_to_df(num_rows_cursor)
+	num_rows = num_rows_df.iloc[0,0]
+
+	# compute predictive probabilities
+	pred_prob_query = "ESTIMATE PREDICTIVE PROBABILITY OF %s AS pred_prob FROM %s WHERE %s._rowid_ >= %d" %(column, population, table, num_rows + start_row)
+	pred_prob_cursor = bdb.execute(pred_prob_query)
+	pred_prob_df = cursor_to_df(pred_prob_cursor)
+	pred_prob_list = df.loc[:, "pred_prob"].tolist()
+
+	# add pred. probs. into the csv
+	data = csv.reader(open('/tmp/%s' %(filename)))
+	lines = [l for l in data]
+	pred_prob_column = 'predictive probability of %s' %(column) + pred_prob_list
+	new_lines = copy.deepcopy(lines)
+	for i, nl in enumerate(new_lines):
+		nl.append(pred_prob_column[i])
+
+	writer = csv.writer(open('/tmp/%s_processed.csv' %(os.path.splitext(os.path.basename(filename))[0]), 'w'))
+	writer.writerows(new_lines)
